b7e8b8416d48fc360c9357f45e58f9cd1578b003
mureyvenom <holuwamurewa@gmail.com>
2021-01-15 22:11:57 +0100

    Monsters rolodex commit

---
+++ src/components/card-list/card-list.component.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './card-list.styles.css';
+import Card from '../card/card.component';
+
+const CardList = ({monsters}) => {
+    return(
+        <div className='card-list'>
+            {
+            monsters.map((monster) => {
+                return <Card monster={monster} key={monster.id} />
+            } )
+            }
+        </div>
+    )
+}
+
+export default CardList;
